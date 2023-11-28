dcc5843c8a48ab6325e5fc690931a268d1a18f4b
Yemata Ferry <yemataferry@gmail.com>
2023-11-28 18:02:14 +0100

    maximum among 2 numbers

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-
+function maxNumber(a, b){
+    if (a > b){
+        return a
+    } else if(b > a){
+        return b
+    } else {
+        return " a and b are equal"
+    }
+}
+console.log(minNumber(14,8))
+console.log(minNumber(2,7))
